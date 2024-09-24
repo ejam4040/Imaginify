7eb9ee4aaf000e82330e1d1cc0b17b69c9676749
ejam4040 <42040610+ejam4040@users.noreply.github.com>
2024-09-24 19:15:22 +0000

    Up to toast. Toast not working.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Sidebar from "@/components/shared/Sidebar"
 import MobileNav from "@/components/shared/mobileNav"
+import { Toaster } from "@/components/ui/toaster"
+
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <main className="root">
@@ -11,6 +13,7 @@ const Layout = ({children}: {children: React.ReactNode}) => {
             {children}
         </div>
       </div>
+      <Toaster/>
     </main>
   )
 }
--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -4,11 +4,19 @@ import { transformationTypes } from '@/constants'
 import { getUserById } from '@/lib/actions/user.actions';
 import { getAuth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import { auth } from "@clerk/nextjs/server";
 
+ 
 const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
 
   const transformation = transformationTypes[type];
+  const { userId } = auth(); 
 
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const user = await getUserById(userId);
 
   return (
     <>
@@ -17,7 +25,13 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
         subtitle={transformation.subTitle}
       />
     
-      <TransformationForm />
+      <TransformationForm 
+      action="Add" 
+      userId={user._Id} 
+      type={transformation.type as TransformationTypeKey}
+      creditBalance={user.creditBalance}
+
+      />
     </>
   )
 }
